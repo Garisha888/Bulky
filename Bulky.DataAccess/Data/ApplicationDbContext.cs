be8f46cd0b2e14f16a095d976d32901d72f84702
Garisha888 <garisha309@gmail.com>
2024-09-23 10:50:46 +0545

    initial commit

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)  //seeding data to database
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+
+
+                );
+            //modelBuilder.Entity<Category>()
+            //    .HasKey(c => c.Id);
+            //var categoryList = new List<Category>
+            //{
+            //     new Category { Id =1, Name= "Action", DisplayOrder=1},
+            //    new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            //    new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            //};
+            //modelBuilder.Entity<Category>().HasData(categoryList);
+            //Category cg = new Category();
+            //cg.Id = 4;  
+            //cg.Name = "abc";
+            //cg.DisplayOrder = 4;
+            //modelBuilder.Entity<Category>().HasData(cg);
+
+            //base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,132 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+			List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+     public IActionResult Create()
+		{
+
+			return View();
+		}
+   
+
+		[HttpPost]
+		public IActionResult Create(Category  obj)
+		{
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+			//if (obj.Name != null && obj.Name.ToLower() == "test")
+			//{
+			//    ModelState.AddModelError("", "Test is an invalid value");
+			//}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
+				//TempData["error"] = "Unable to create";
+				return RedirectToAction("Index");
+			}
+			
+			return View();
+
+		}
+		
+		public IActionResult Edit(int? Id)
+		{
+			if (Id == null || Id == 0)
+			{
+
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(Id);
+			// Category? categoryFromDb1 =_db.Categories.FirstOrDefault(u=>u.Id==id);
+			// Category? categoryFromDb2 =_db.Categories.where(u=>u.Id==id).FirstOrDefault
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+            
+
+			if (ModelState.IsValid)
+			{
+				//var category = _db.Categories.Find(obj.Id);
+				//if (category == null) { 
+				//	return NotFound();
+				//}
+				//category.Name = string.IsNullOrEmpty(obj.Name)? category.Name: obj.Name;
+				//category.DisplayOrder = obj.DisplayOrder == 0 ? category.DisplayOrder : obj.DisplayOrder;
+
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
+
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+
+
+		public IActionResult Delete(int? Id)
+		{
+			if (Id == null || Id == 0)
+			{
+
+				return NotFound();
+			}
+			Category categoryFromDb = _db.Categories.Find(Id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? Id)
+		{
+
+			Category? obj = _db.Categories.Find(Id);
+
+			if (obj == null )
+			{
+
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+
+		}
+
+
+
+	}
+}
